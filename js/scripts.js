40562620dcbf1f18da57495c052c3fecf3413e92
Chris Kuiper and Schuyler Klaassen <chris.kuiper@gmail.com>
2016-01-20 11:29:39 -0800

    create spec for moving first consonant to end adding ay to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-var pigLatin = function(vowel) {
-  return (vowel + "ay")
+var pigLatin = function(text) {
+  if(beginConsonant(text)) {
+    var newText = text.slice(0,1);
+      return text.substring(1,text.length) + newText + "ay";
+      } else {
+    return (text + "ay");
+  }
+}
+
+var beginConsonant = function(text) {
+  if(text[0] !== 'a' && text[0]!== 'e' && text[0]!== 'i' && text[0]!== 'o' && text[0]!== 'u') {
+    return true;
+  } else {
+    return false;
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,18 @@ describe('pigLatin', function() {
   it("will add 'ay' to the end of words that start with a vowel", function() {
     expect(pigLatin("apple")).to.equal("appleay");
   });
+
+
+  it("will move any first consonant from the beginning of a word to the end, and add 'ay'.", function() {
+    expect(pigLatin("chair")).to.equal("haircay");
+  });
+});
+
+describe('beginConsonant', function() {
+  it("will determine if the first letter of a word is a consonant", function() {
+    expect(beginConsonant("cat")).to.equal(true);
+  });
+  it("will determine if the first letter of a word is a consonant", function() {
+    expect(beginConsonant("at")).to.equal(false);
+  });
 });
